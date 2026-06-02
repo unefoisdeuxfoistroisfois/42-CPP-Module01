25f6628124cac7f9df2ec6ef0309732bae37216b
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-06-02 14:08:39 +0200

    ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,62 @@
+#include "Harl.hpp"
+
+Harl::Harl(){
+    std::cout << "Harl is ready to complain..." << std::endl;
+}
+
+Harl::~Harl(){
+    std::cout << "Harl has been silenced." << std::endl;
+}
+
+void    Harl::debug( void ){
+    std::cout << " I love having extra bacon for my ";
+    std::cout << "7XL-double-cheese-triple-pickle-special- ";
+   std::cout << "ketchup burger. I really do!" << std::endl;
+}
+
+void    Harl::info(void){
+    std::cout << "I cannot believe adding extra bacon costs more money.";
+    std::cout << " You didn’t put enough bacon in my burger!";
+    std::cout << " If you did, I wouldn’t be asking for more!" << std::endl;
+}
+        
+void    Harl::warning( void ){
+    std::cout << "I think I deserve to have some extra bacon for free.";
+    std::cout << "I’ve been coming for years, ";
+    std::cout << "whereas you started working here just last month." << std::endl;
+}
+
+void    Harl::error( void ){
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain( std::string level ){
+    //if (level == "DEBUG")
+    //    debug();
+    //else if (level == "INFO")
+    //    info();
+    //else if (level == "WARNING")
+    //    warning();
+    //else if (level == "ERROR")
+    //    error();
+
+    /*
+     * Tableau de 4 pointeur vers
+     * des fonctions membre de Harl
+     * qui retourne void et qui prend void
+     */
+    void (Harl::*tabPTRS[4])(void) = {
+        &Harl::debug, // & ppour pouvoir avoir accese a l'adresse pour polus tard
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+    std::string complains[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 4; i ++){
+
+        if (complains[i] == level){
+            (this->*tabPTRS[i])(); // il faut this->* pour appele l'objet Harl qui lui a les fonctions
+        }
+    }
+}
